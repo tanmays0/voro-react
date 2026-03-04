7ea8e16b1df182e4868494e23f248d9873b431a9
tanmays0 <shinde.tanmay@gmail.com>
2026-03-04 06:13:17 +0530

    Add deploy script for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true
+  base: '/voro-react/',
+  server: {
+    host: true
   }
 })
